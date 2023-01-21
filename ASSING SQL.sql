cd0f8efed9effc3d89e0f352099da7890abb44c4
hardik1620 <113962588+hardik1620@users.noreply.github.com>
2023-01-21 09:22:25 +0530

    Add files via upload

---
+++ ASSING SQL.sql
@@ -0,0 +1,6 @@
+use assingment;
+SELECT * FROM assingment.assin_sql1;
+select max(PRICE) from assin_sql1;
+select * from assin_sql1 order by price desc;
+select * from assin_sql1 where price >= 448;
+
